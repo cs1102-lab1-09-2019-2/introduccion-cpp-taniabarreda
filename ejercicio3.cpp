2d48ebc43b2b8782bd260f494e1d09d42fdbd7b1
taniabarreda <51935510+taniabarreda@users.noreply.github.com>
2019-08-23 15:05:36 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,7 +2,9 @@
 using std::cout;
 
 int main() {
-    for (int i = 0; i < ; i++) {
-        //imprimir los 10 numeros [0-9]
+    for (int i = 0; i < 10; i++) {
+        cout<<i<<endl;
+        
     }
+    return 0;
 }
