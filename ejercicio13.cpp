70804b277053511b01524e269efe5045dda2ccce
taniabarreda <51935510+taniabarreda@users.noreply.github.com>
2019-08-23 23:23:51 -0500

    Update ejercicio13.cpp

--- ejercicio13.cpp
+++ ejercicio13.cpp
@@ -1,12 +1,13 @@
+#include<iostream>
+using namespace std;
 
 int main() {
-
     float base, altura;
     cout<<"Ingrese la base del triangulo: ";
     cin>>base;
     cout<<"Ingrese la altura del triangulo: ";
     cin>>altura;
-    float area = //calcular el area
+    float area = base*altura;//calcular el area
     cout<<"El area del triangulo es: "<<area<<"\n";
 
 }
