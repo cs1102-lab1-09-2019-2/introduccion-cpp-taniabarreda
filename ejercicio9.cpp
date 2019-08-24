96721cfa19edf59d0b5b0d55a7d69351654a3bcd
taniabarreda <51935510+taniabarreda@users.noreply.github.com>
2019-08-23 23:14:20 -0500

    Update ejercicio9.cpp

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -5,9 +5,10 @@ int main() {
 
     int i = 1;
     while (i <= 10) {
-        if (   ) {
-            //imprimir numeros pares verticalmente
+        if ( i % 2 == 0  ) {
+            cout<< i<<"\n";//imprimir numeros pares verticalmente
         }
-
+      i=i+1;
     }
+return 0;
 }
