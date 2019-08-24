8c0ee4d1d04305b1470b6cf0cbc31e77bc51a772
taniabarreda <51935510+taniabarreda@users.noreply.github.com>
2019-08-24 12:06:09 -0500

    Update ejercicio8.cpp

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -4,9 +4,11 @@ using std::cout;
 int main() {
     auto i = 0;
     // Mostrar los numeros del 0 al 4
-    while (i < ) {
+    while (i < 5 ) {
         cout<< i <<"\n";
+        i=i+1;
 
     }
 }
+
 
