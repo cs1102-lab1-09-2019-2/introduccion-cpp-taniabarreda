c01600e1d2ecba4f9718685cd95de6d5e2bd2dae
taniabarreda <51935510+taniabarreda@users.noreply.github.com>
2019-08-23 23:20:09 -0500

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
-#include <string>
-using namespace str;
+using namespace std;
 int main() {
-    int i = 1;
-    str c;
+    int i = 1, c ;
 
     // Post-incremento asigna i a c y luego incrementa i
     c = i++;
+  
     cout<<"Post-incremento ejemplo:\n";
     cout<<"El valor de c es: "<<c<<"\n";
     cout<<"El valor de i es: "<<i<<"\n";
     cout<<"\n";
     // resetear i a 1
     i = 1;
-
     // Pre-incremento incrementa i, luego asigna a c
     c = ++i;
-
     cout<<"Pre-incremento ejemplo:\n";
     cout<<"El valor de c es: "<<c<<"\n";
     cout<<"El valor de i es: "<<i<<"\n";
